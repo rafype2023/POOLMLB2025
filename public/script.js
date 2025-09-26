fff094ad00563c5d3edb8ce9bc2d81b364318577
Rafael Perez <rafaelperez@MacBook-Pro-2.local>
2025-09-26 19:04:23 -0400

    rev wildcard5

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   // --- 1. CONFIGURATION ---
-  const alSeeds = ["Seattle Mariners", "Toronto BlueJays", "Cleveland Guardians","New York Yankees","Boston Red Sox", "Detroit Tigers"];
+  const alSeeds = ["Toronto BlueJays","Seattle Mariners","Cleveland Guardians","New York Yankees","Boston Red Sox", "Detroit Tigers"];
   const nlSeeds = ["Philadelphia Phillies", "Milwaukee Brewers", "Chicago Cubs", "LA Dodgers", "New York Mets", "San Diego Padres"];
   
   const teamLogos = {
